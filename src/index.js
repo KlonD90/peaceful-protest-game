e65af14a434f5bd76622928d64a210c6a936789e
klond90 <newbiecraft@gmail.com>
2018-01-07 14:54:02 +0300

    change renderer check

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function init() {
       height: containerNode.clientHeight,
       parent: containerNode,
       antialias: true,
-      renderer: Phaser.CANVAS
+      renderer: Phaser.WEBGL
       // resolution: window.devicePixelRatio || 1
   });
 
