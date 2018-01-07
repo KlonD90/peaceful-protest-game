8587decef5e991b39c00872206406628660cdad8
klond90 <newbiecraft@gmail.com>
2018-01-07 15:03:21 +0300

    canvas renderer

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function init() {
       height: containerNode.clientHeight,
       parent: containerNode,
       antialias: true,
-      renderer: Phaser.WEBGL_MULTI
+      renderer: Phaser.CANVAS
       // resolution: window.devicePixelRatio || 1
   });
 
