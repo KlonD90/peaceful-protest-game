a5374f3957828199e5a6f196233cba72b41420f5
Константин <rondo.devil@gmail.com>
2018-01-20 20:38:26 +0700

    Почистил класс прогрессбара

--- dist/bundle.js
+++ dist/bundle.js
@@ -1638,7 +1638,8 @@ var ProgressBar = function () {
                     width = _ref.width,
                     color = _ref.color,
                     lineWidth = _ref.lineWidth,
-                    game = _ref.game;
+                    game = _ref.game,
+                    sprite_type = _ref.sprite_type;
 
                 _classCallCheck(this, ProgressBar);
 
@@ -1650,41 +1651,18 @@ var ProgressBar = function () {
                 this.graphics.y = -30;
                 this.graphics.x = -this.fullWidth / 2;
 
-                this.radius = radius;
-                this.width = width;
+                //this.radius = radius;
+                //this.width = width;
                 this.color = color;
-                this.lineWidth = lineWidth;
+                //this.lineWidth = lineWidth;
 
-                /*
-                let texture = CACHED_TEXTURE;
-                
-                if(texture == null){
-                    let restMainPart = this.game.add.graphics(0,0);
-                      restMainPart.beginFill(0xffffff);
-                    this.drawFigure(restMainPart);
-                    restMainPart.endFill();
-                    
-                    texture = restMainPart.generateTexture();
-                    restMainPart.destroy();
-                    CACHED_TEXTURE = texture;
-                    console.log("PROGRESS_CREATE TEXTURE");
-                }*/
+                var s = sprite_type ? sprite_type : "small_progress";
 
-                var bg = game.add.sprite(0, 0, "ALL_IMAGES", "small_progress");
+                var bg = game.add.sprite(0, 0, "ALL_IMAGES", s);
 
-                this.graphics.addChild(bg); //this.restMainPart);
+                this.graphics.addChild(bg);
 
-                //this.fillPart = this.game.add.group();
-
-
-                //this.fillMainPart = this.game.add.graphics().beginFill(this.color);
-                //this.drawFigure(this.fillMainPart);
-                //this.fillMainPart.endFill();
-
-                //this.fillTexture = new PIXI.Texture(texture.baseTexture,
-                //                   new Phaser.Rectangle(0,0, width, radius * 2));
-
-                this.fillSprite = game.add.sprite(0, 0, "ALL_IMAGES", "small_progress");
+                this.fillSprite = game.add.sprite(0, 0, "ALL_IMAGES", s);
                 this.fillSprite.tint = this.color;
 
                 this.graphics.addChild(this.fillSprite);
@@ -1692,35 +1670,13 @@ var ProgressBar = function () {
                 this.cropRect = new Phaser.Rectangle(0, 0, width / 2, radius * 2); //width);
                 this.fillSprite.crop(this.cropRect);
 
-                //this.fillMainPart.destroy();
-
-                //this.graphics.addChild(sprite);//this.restMainPart);
-
-
-                //this.fillMask = this.game.add.graphics();
-
-                //this.fillPart.addChild(this.fillSprite);
-
-                //this.fillPart.addChild(this.fillMask);
-
-                //this.fillSprite.mask = this.fillMask;
-                //this.fillMainPart.mask = this.fillMask;
                 this.graphics.visible = false;
-                // this.restPart = this.game.add.group();
-
-
-                // this.restPart.addChild(this.restMainPart);
-                // this.restMask = this.game.add.graphics();
-                // this.restPart.addChild(this.restMask);
-                // this.restMainPart.mask = this.restMask;
-                // this.restMask.clear().beginFill(0).drawRect(0,0,0,0).endFill();
         }
 
         _createClass(ProgressBar, [{
                 key: "update",
                 value: function update(percent) {
 
-                        //this.restMask.clear();
                         percent = this.game.math.clamp(percent, 0, 1);
 
                         if (percent !== 0) {
@@ -1733,37 +1689,15 @@ var ProgressBar = function () {
                                 this.graphics.visible = true;
 
                                 this.lastWidth = fillWidth;
-                                //this.fillMask.clear();
-                                //this.fillMask.beginFill(0).drawRect(0, 0, fillWidth, this.radius * 2).endFill();
-
 
                                 this.cropRect.width = fillWidth;
                                 this.fillSprite.updateCrop();
-
-                                // this.restMask.beginFill(0).drawRect(fillWidth, 0, restWidth, this.radius * 2).endFill();
-                                // this.restPart.x = fillWidth;
-                                // this.restPart.mask = this.restMask;
-                                // this.fillPart.mask = this.fillMask;
                         } else {
                                 if (this.lastWidth === 0) return;
 
                                 this.lastWidth = 0;
                                 this.graphics.visible = false;
-
-                                // this.fillMask.beginFill(0).drawRect(0, 0, 0, this.radius * 2).endFill();
-                                // this.restMask.beginFill(0).drawRect(0, 0, 0, this.radius * 2).endFill();
                         }
-                        // this.fillPart.mask = this.fillMask;
-                        // this.restPart.mask = this.restMask;
-                }
-        }, {
-                key: "drawFigure",
-                value: function drawFigure(graphic) {
-                        graphic
-                        // .lineStyle(this.lineWidth, this.color, 1)
-                        .moveTo(this.radius, 0).lineTo(this.radius + this.width, 0).arc(this.radius + this.width, this.radius, this.radius, -Math.PI / 2, Math.PI / 2).lineTo(this.radius, this.radius * 2).arc(this.radius, this.radius, this.radius, Math.PI / 2, 3 * Math.PI / 2)
-                        // .lineTo(this.radius, 0)
-                        .lineTo(this.radius + this.width, 0);
                 }
         }]);
 
--- src/objects/ProgressBar.js
+++ src/objects/ProgressBar.js
@@ -2,7 +2,7 @@
 //let CACHED_TEXTURE = null;
 
 export default class ProgressBar{
-    constructor({radius, width, color, lineWidth, game}){
+    constructor({radius, width, color, lineWidth, game, sprite_type}){
       
         this.lastWidth = 0;
         this.game = game;
@@ -12,42 +12,18 @@ export default class ProgressBar{
         this.graphics.y = -30;
         this.graphics.x = -this.fullWidth/2;
         
-        this.radius = radius;
-        this.width = width;
+        //this.radius = radius;
+        //this.width = width;
         this.color = color;
-        this.lineWidth = lineWidth;
-
-        /*
-        let texture = CACHED_TEXTURE;
+        //this.lineWidth = lineWidth;
         
-        if(texture == null){
-            let restMainPart = this.game.add.graphics(0,0);
+        let s = sprite_type ? sprite_type :"small_progress";
 
-            restMainPart.beginFill(0xffffff);
-            this.drawFigure(restMainPart);
-            restMainPart.endFill();
-            
-            texture = restMainPart.generateTexture();
-            restMainPart.destroy();
-            CACHED_TEXTURE = texture;
-            console.log("PROGRESS_CREATE TEXTURE");
-        }*/
+        let bg = game.add.sprite(0,0,"ALL_IMAGES",s);
 
-        let bg = game.add.sprite(0,0,"ALL_IMAGES","small_progress");
+        this.graphics.addChild(bg);
 
-        this.graphics.addChild(bg);//this.restMainPart);
-
-        //this.fillPart = this.game.add.group();
-        
-        
-        //this.fillMainPart = this.game.add.graphics().beginFill(this.color);
-        //this.drawFigure(this.fillMainPart);
-        //this.fillMainPart.endFill();
-
-        //this.fillTexture = new PIXI.Texture(texture.baseTexture,
-//                   new Phaser.Rectangle(0,0, width, radius * 2));
-
-        this.fillSprite = game.add.sprite(0,0,"ALL_IMAGES","small_progress");
+        this.fillSprite = game.add.sprite(0,0,"ALL_IMAGES", s);
         this.fillSprite.tint = this.color;
         
         this.graphics.addChild(this.fillSprite);
@@ -55,33 +31,11 @@ export default class ProgressBar{
 
         this.cropRect = new Phaser.Rectangle(0, 0, width / 2, radius * 2);//width);
         this.fillSprite.crop(this.cropRect);
-        
-        //this.fillMainPart.destroy();
 
-        //this.graphics.addChild(sprite);//this.restMainPart);
-
-
-        //this.fillMask = this.game.add.graphics();
-
-        //this.fillPart.addChild(this.fillSprite);
-
-        //this.fillPart.addChild(this.fillMask);
-        
-        //this.fillSprite.mask = this.fillMask;
-        //this.fillMainPart.mask = this.fillMask;
         this.graphics.visible = false;
-        // this.restPart = this.game.add.group();
-
-
-        // this.restPart.addChild(this.restMainPart);
-        // this.restMask = this.game.add.graphics();
-        // this.restPart.addChild(this.restMask);
-        // this.restMainPart.mask = this.restMask;
-        // this.restMask.clear().beginFill(0).drawRect(0,0,0,0).endFill();
     }
     update(percent){
-
-        //this.restMask.clear();
+        
         percent = this.game.math.clamp(percent, 0, 1);
 
         if (percent !== 0) {
@@ -96,17 +50,9 @@ export default class ProgressBar{
             
 
             this.lastWidth = fillWidth;
-            //this.fillMask.clear();
-            //this.fillMask.beginFill(0).drawRect(0, 0, fillWidth, this.radius * 2).endFill();
-            
 
             this.cropRect.width = fillWidth;
             this.fillSprite.updateCrop();
-
-            // this.restMask.beginFill(0).drawRect(fillWidth, 0, restWidth, this.radius * 2).endFill();
-            // this.restPart.x = fillWidth;
-            // this.restPart.mask = this.restMask;
-            // this.fillPart.mask = this.fillMask;
         }
         else
         {
@@ -115,22 +61,6 @@ export default class ProgressBar{
             
             this.lastWidth = 0;
             this.graphics.visible = false;
-
-            // this.fillMask.beginFill(0).drawRect(0, 0, 0, this.radius * 2).endFill();
-            // this.restMask.beginFill(0).drawRect(0, 0, 0, this.radius * 2).endFill();
         }
-        // this.fillPart.mask = this.fillMask;
-        // this.restPart.mask = this.restMask;
-    }
-    drawFigure(graphic){
-        graphic
-        // .lineStyle(this.lineWidth, this.color, 1)
-        .moveTo(this.radius, 0)
-        .lineTo(this.radius + this.width, 0)
-        .arc(this.radius + this.width, this.radius, this.radius, -Math.PI/2, Math.PI/2)
-        .lineTo(this.radius, this.radius*2)
-        .arc(this.radius, this.radius, this.radius, Math.PI/2, 3*Math.PI/2)
-        // .lineTo(this.radius, 0)
-        .lineTo(this.radius + this.width, 0)
     }
 }
